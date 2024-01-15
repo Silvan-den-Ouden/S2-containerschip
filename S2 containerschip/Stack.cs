086541a803d4a15f21e629eb7a00fcb1e8275a28
Ouden,Silvan S. den <silvan.denouden@student.fontys.nl>
2024-01-15 20:16:23 +0100

    stack functionality changed

--- S2 containerschip/Stack.cs
+++ S2 containerschip/Stack.cs
@@ -3,9 +3,11 @@
     public class Stack
     {
         public List<Container> Containers { get; set; }
+        public bool HasValuable { get ; set; }
 
         public Stack() {
             Containers = new();
+            HasValuable = false;    
         }
 
         public void AddContainer(Container container)
@@ -21,12 +23,7 @@
 
         public bool CanAddContainerToStack(Container container)
         {
-            if(GetLoadOnBottomContainer() + container.GetWeight() > container.GetMaxLoad())
-            {
-                return false;
-            }
-
-            if (TopIsValuable())
+            if(GetStackWeight() > container.GetMaxLoad())
             {
                 return false;
             }
@@ -34,32 +31,15 @@
             return true;
         }
 
-        public int GetLoadOnBottomContainer()
+        public int GetStackWeight()
         {
             int load = 0;
 
-            for(int i = 1; i < Containers.Count; i++) {
+            for(int i = 0; i < Containers.Count; i++) {
                 load += Containers[i].GetWeight();
             }
 
             return load;
-        }
-
-        public bool TopIsValuable()
-        {
-            // if there are no containers, the top cant be valuable
-            if(Containers.Count == 0)
-            {
-                return false;
-            }
-
-            // checks if last (aka the top of the stack) container in containers is valuable 
-            if (Containers[^1].Valuable)
-            {
-                return true;
-            }
-
-            return false;
         }
     }
 }
